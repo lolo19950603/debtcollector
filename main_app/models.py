211891906e2dac4c95974708e46341c5f0cfd941
loren950603 <loren840603@gmail.com>
2022-09-28 20:35:15 -0400

    add models to database and create admin user

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Debtor(models.Model):
+    name = models.CharField(max_length=100)
+    debts = models.IntegerField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Debtor
 
-class Debtor:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, debts, age):
-    self.name = name
-    self.debts = debts
-    self.age = age
-
-debtors = [
-  Debtor('Lolo', 5000000, 53),
-  Debtor('Sachi', 250456, 20),
-  Debtor('Candy', 8446511555, 18)
-]
 
 # Create your views here.
 def home(request):
@@ -22,4 +12,5 @@ def about(request):
 
 # Add new view
 def debtors_index(request):
+  debtors = Debtor.objects.all()
   return render(request, 'debtors/index.html', { 'debtors': debtors })
